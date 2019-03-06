72e4a5aa24f8c603bce7d0fd1ae2c9c7549d9358
Ueverton21 <uevertonandrade@gmail.com>
2019-03-05 21:44:16 -0300

    Removido teste de comentario

--- src/actions/types.js
+++ src/actions/types.js
@@ -13,5 +13,3 @@ export const LOAD_LOGIN = 'LOAD_LOGIN';
 export const LOGIN_SUCESSO = 'LOGIN_SUCESSO';
 export const LOGIN_ERRO = 'LOGIN_ERRO';
 export const LOGOUT = 'LOGOUT';
-
-export const TESTE_COMENTARIO= 'TESTE_COMENTARIO';
